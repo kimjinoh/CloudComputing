42f2c32119a2b3f140a57f13285a310ce8eaf3c6
SungYuJeong <49743005+SungYuJeong@users.noreply.github.com>
2019-11-26 23:32:30 +0900

    Add files via upload

---
+++ cgv.py
@@ -0,0 +1,17 @@
+import urllib.request
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=03,205&theatercode=0228&date=20191126'
+res = urllib.request.urlopen(url).read()
+movie = BeautifulSoup(res, 'html.parser')
+
+movie = movie.findAll("div", class_="col-times")
+
+#print(movie)
+
+out=open("outputs.html",'w', -1, "utf-8")
+
+for ul in movie:
+    print(ul, file=out)
+
