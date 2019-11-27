0aca1009377400f2e486196bb4dc7b6426071a86
kimjinoh <wlsdh1110@gmail.com>
2019-11-28 04:16:44 +0900

    Add files via upload
    11월28일~12월1일 영화시간표

--- lotcinema.py
+++ lotcinema.py
@@ -10,21 +10,34 @@ options = Options()
 options.headless = True
 chrome_driver_path = 'C:/Users/wlsdh/git/python/chromedriver.exe'
 driver = webdriver.Chrome(executable_path = chrome_driver_path, options=options)
+#driver = webdriver.Chrome(chrome_driver_path)
 url = 'http://www.lottecinema.co.kr/LCHS/Contents/ticketing/ticketing.aspx#%EC%A0%84%EC%B2%B4'
-
+lotdate_file_path = 'C:/Users/wlsdh/git/python/python_crawler_example_01/practice/lottecinemacrawl/'
+lotdate_file_name = 'lotdate.txt'
 driver.get(url)
 bsObj = BeautifulSoup(driver.page_source, 'html.parser')
-
-date = driver.find_element_by_xpath("//label[@for='November27']")
-date.click()
-linka = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/span/a")
-linka.click()
-aaaa = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/div/ul/li[6]/a")
-aaaa.click()
-time.sleep(3)
 out=open("lotte.html",'w', -1, "utf-8")
-location = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[3]/div[2]")
+def time_request():
+    lotdate_file = open(lotdate_file_path + lotdate_file_name, 'r')
+    date = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[3]/fieldset/div/label[2]")
+    date.click()
+    linka = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/span/a")
+    linka.click()
+    aaaa = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/div/ul/li[6]/a")
+    aaaa.click()
+    time.sleep(3)
+    location = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[3]/div[2]")
 
-for p in location.find_elements_by_tag_name('dl'):
-    print(p.text.split("\n"), file = out)
-    print("\n", file = out)
+    for p in location.find_elements_by_tag_name('dl'):
+        print(p.text.split("\n"), file = out)
+        print("\n", file = out)
+    for lotdate_code in lotdate_file.readlines():
+        date = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[3]/fieldset/div/label["+lotdate_code+"]")
+        date.click()
+        time.sleep(3)
+        location = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[3]/div[2]")
+        for q in location.find_elements_by_tag_name('dl'):
+            print(q.text.split("\n"), file=out)
+            print("\n", file=out)
+
+time_request()
