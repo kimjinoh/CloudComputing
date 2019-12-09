633d5a2d9e76567a9db5ee84396ba0e6b88b5ed3
SungYuJeong <49743005+SungYuJeong@users.noreply.github.com>
2019-12-09 13:03:08 +0900

    repeat add

--- ltcgvmegacrawl.py
+++ ltcgvmegacrawl.py
@@ -1,27 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 import datetime
 import re
 import urllib.request
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta
+import threading
+
 import time
 
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
-# from pandas import Series, DataFrame
-# import pandas as pd
-# import numpy as np
-
-# import openpyxl
-# wb = openpyxl.Workbook()
-#out = wb.save('test.xlsx')
-
-# options = Options()
-# options.headless = True
+options = Options()
+options.headless = True
 chrome_driver_path = 'C:/Users/tjddb/Downloads/chromedriver_win32/chromedriver.exe'
-# driver = webdriver.Chrome(executable_path = chrome_driver_path, options=options)
-driver = webdriver.Chrome(chrome_driver_path)
+driver = webdriver.Chrome(executable_path = chrome_driver_path, options=options)
+# driver = webdriver.Chrome(chrome_driver_path)
 
-out = open("timetable.html",'w', -1, "utf-8")
+def fun():
+    timer = threading.Timer(180, fun)
+    global out
+    out = open("timetable.txt", 'w', -1, "utf-8")
+    cgvtime_request()
+    lottime_request()
+    megatime_request()
+    timer.start()
+
 def lottime_request():
     url = 'http://www.lottecinema.co.kr/LCHS/Contents/ticketing/ticketing.aspx#%EC%A0%84%EC%B2%B4'
     driver.get(url)
@@ -31,20 +35,52 @@ def lottime_request():
     linka = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/span/a")
     linka.click()
     time.sleep(3)
-
+    lt_id = 1
     loc_arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     for s in loc_arr:
+        if s == '1':
+            loc_name = '대전(백화점)점.'
+            branch = '대전.'
+        elif s == '2':
+            loc_name = '대전관저점.'
+            branch = '대전.'
+        elif s == '3':
+            loc_name = '대전둔산(월평동)점.'
+            branch = '대전.'
+        elif s == '4':
+            loc_name = '대전센트럴점.'
+            branch = '대전.'
+        elif s == '5':
+            loc_name = '서산점.'
+            branch = '충남.'
+        elif s == '6':
+            loc_name = '서청주(아울렛)점.'
+            branch = '충북.'
+        elif s == '7':
+            loc_name = '아산터미널점.'
+            branch = '충남.'
+        elif s == '8':
+            loc_name = '청주(성안길)점.'
+            branch = '충북.'
+        elif s == '9':
+            loc_name = '청주용암점.'
+            branch = '충북.'
+        elif s == '10':
+            loc_name = '충주점.'
+            branch = '충북.'
+
         loc_click = driver.find_element_by_xpath("//*[@id='content']/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div[1]/ul/li[3]/div/ul/li[" + s + "]/a")
         loc_click.click()
         time.sleep(1)
-        location = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[3]/div[2]")
+        location = driver.find_element_by_xpath("//*[@id='content']/div[3]/div[3]")
+        time.sleep(1)
         for p in location.find_elements_by_tag_name('dl'): # 처음 영화데이터 출력
             result_seta = []  #
             result = ""
             final = []
             a = p.text.split(":")
             title = re.split('\n', a[0])[0][2:]
-            print(title)
+            #print(('롯데시네마.' + branch + loc_name + title).split("."), file = out)
             for i in a:
                 b = i[0:2] + i[-2:]
                 result_seta.append(b)
@@ -63,25 +99,75 @@ def lottime_request():
                     if i == ' : ':
                         final.remove(' : ')
             sortlist = sorted(final, key=lambda x: (x))
-            print(sortlist)
+            data = []
+            for abc in sortlist:
+                data.append(("L." + str(lt_id) + ".롯데시네마." + branch + loc_name + title) + "." + abc)
+                lt_id += 1
+            for abc in range(0, len(sortlist)):
+                print(data[abc], file=out)
+            #print(sortlist, file = out)
+            data.clear()
             sortlist.clear()
             final.clear()
             result_seta.clear()
         loc_click.click()
-
+        time.sleep(1)
 
 def cgvtime_request():
 
     # 현재 날짜 구하기
-    today=datetime.today()
+    today= datetime.today()
     # 내일 날짜 구하기
     tomorrow= today + timedelta(days=1)
     tomorrowDate=tomorrow.strftime('%Y%m%d')
-    print(tomorrowDate)
+    #print(tomorrowDate, file = out)
     date_arr = [tomorrowDate]
+    cgv_id = 1;
     cgv_arr = ['0207', '0007', '0127', '0275', '0091', '0219', '0209', '0044', '0293', '0228', '0297', '0282', '0142', '0294']
-    #          공주     대전   대전터미널 보령    서산     세종   유성온천  천안  천안터미널 청주(서문) 청주성안길 청주율량 청주지웰 청주터미널
+    #          당진     대전   대전터미널 보령    서산     세종   유성온천  천안  천안터미널 청주(서문) 청주성안길 청주율량 청주지웰 청주터미널
     for cgvcode_code in cgv_arr:
+        if cgvcode_code == '0207':
+            loc_name = '당진점.'
+            branch = '충남.'
+        elif cgvcode_code == '0007':
+            loc_name = '대전점.'
+            branch = '대전.'
+        elif cgvcode_code == '0127':
+            loc_name = '대전터미널점.'
+            branch = '대전.'
+        elif cgvcode_code == '0275':
+            loc_name = '보령점.'
+            branch = '충남.'
+        elif cgvcode_code == '0091':
+            loc_name = '서산점.'
+            branch = '충남.'
+        elif cgvcode_code == '0219':
+            loc_name = '세종점.'
+            branch = '세종.'
+        elif cgvcode_code == '0209':
+            loc_name = '유성온천점.'
+            branch = '대전.'
+        elif cgvcode_code == '0044':
+            loc_name = '천안점.'
+            branch = '충남.'
+        elif cgvcode_code == '0293':
+            loc_name = '천안터미널점.'
+            branch = '충남.'
+        elif cgvcode_code == '0228':
+            loc_name = '청주(서문)점.'
+            branch = '충북.'
+        elif cgvcode_code == '0297':
+            loc_name = '청주성안길점.'
+            branch = '충북.'
+        elif cgvcode_code == '0282':
+            loc_name = '청주율량점.'
+            branch = '충북.'
+        elif cgvcode_code == '0142':
+            loc_name = '청주지웰점.'
+            branch = '충북.'
+        elif cgvcode_code == '0294':
+            loc_name = '청주터미널점.'
+            branch = '충북.'
         url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=03,205&theatercode=' + cgvcode_code
         for date_code in date_arr:
             url_c = url+'&date='+ date_code
@@ -97,7 +183,7 @@ def cgvtime_request():
                 final = []
                 title = ul.text.replace('\r', '').replace(' ', '').split("\n")
                 arr_title = ul.text.replace('\r', '').replace(' ', '').split(":")
-                print(title[4])
+                #print(("CGV."+ branch + loc_name+title[4]).split("."), file = out)
                 for i in arr_title:
                     b = i[0:2] + i[-2:]
                     result_seta.append(b)
@@ -115,8 +201,15 @@ def cgvtime_request():
                     for i in final:
                         if i == ' : ':
                             final.remove(' : ')
+
                 sortlist = sorted(final, key=lambda x: (x))
-                print(sortlist)
+                data = []
+                for abc in sortlist:
+                    data.append(("C."+str(cgv_id)+".CGV."+ branch + loc_name+title[4])+"."+abc)
+                    cgv_id += 1
+                for abc in range(0,len(sortlist)):
+                    print(data[abc], file = out)
+                data.clear()
                 sortlist.clear()
                 final.clear()
                 result_seta.clear()
@@ -130,25 +223,66 @@ def megatime_request():
     linka.click()
     calander = driver.find_element_by_xpath("//*[@id='container']/div[2]/div/div[1]/div[1]/div[2]/button[2]")
     calander.click()
-    loc_arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    mega_id = 1
+    loc_arr = ['1', '2', '3',    '4', '5','6','7', '8', '9']
+    #          공주 대전 대전중앙로 세종 오창 제천 진천 천안 청주충북대
     for i in loc_arr:
+        if i == '1':
+            loc_name = '공주점.'
+            branch = '충남.'
+        elif i == '2':
+            loc_name = '대전점.'
+            branch = '대전.'
+        elif i == '3':
+            loc_name = '대전중앙로점.'
+            branch = '대전.'
+        elif i == '4':
+            loc_name = '세종점.'
+            branch = '세종.'
+        elif i == '5':
+            loc_name = '오창점.'
+            branch = '충북.'
+        elif i == '6':
+            loc_name = '제천점.'
+            branch = '충북.'
+        elif i == '7':
+            loc_name = '진천점.'
+            branch = '충북.'
+        elif i == '8':
+            loc_name = '천안점.'
+            branch = '충남.'
+        elif i == '9':
+            loc_name = '청주충북대점.'
+            branch = '충북.'
         loc_click = driver.find_element_by_xpath("//*[@id='region_45']/li[" + i + "]/a")
         loc_click.click()
         location = driver.find_element_by_xpath("//*[@id='timeTableCinemaList']")
-        for p in location.find_elements_by_tag_name('tr'):
+        locations = location.find_elements_by_tag_name('tr')
+        final = []
+        res_arr = []
+        sortlist = []
+        for p in locations:
             result_seta = []
             result = ""
-            final = []
             a = p.text.split(":")
-            # print(a)
+            if(p.find_elements_by_class_name("title")):
+                if(sortlist!=[]):
+                    data = []
+                    for abc in sortlist:
+                        data.append(("M." + str(mega_id) + title_arr) + "." + abc)
+                        mega_id += 1
+                    for abc in range(0, len(sortlist)):
+                        print(data[abc], file = out)
+                    #print(sortlist, file = out)
             for q in p.find_elements_by_class_name("title"):
                 zz = q.text.split("\n")
                 title = re.split('\n', zz[1])[0][0:]
-                print(title)
+                title_arr = ".메가박스."+ branch + loc_name + title
+                #print(title_arr.split("."), file = out)
+                final.clear()
             for i in a:
                 b = i[0:2] + i[-2:]
                 result_seta.append(b)
-
             for j in result_seta:
                 result += j
             time1 = result[2:-2]
@@ -157,22 +291,27 @@ def megatime_request():
                 korean = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
                 dab2 = re.sub(korean, '', dab)
                 final.append(dab2[0:2] + ':' + dab2[-2:])
-            # for r in p.find_elements_by_class_name("room"):
             for i in final:
                 if i == ' : ':
                     final.remove(' : ')
-                # r.text.split("\n")  #영화 상영관 정보
-            res_arr = []
             res_arr += final
             sortlist = sorted(res_arr, key=lambda x: (x))
-            print(sortlist)
-            final.clear()
+            res_arr.clear()
             result_seta.clear()
-        res_arr.clear()
+        data = []
+        for abc in sortlist:
+            data.append(("M." + str(mega_id) + title_arr) + "." + abc)
+            mega_id += 1
+        for abc in range(0, len(sortlist)):
+            print(data[abc], file = out)
+        #print(sortlist, file = out)
         sortlist.clear()
         time.sleep(3)
 
 
-cgvtime_request()
-lottime_request()
-megatime_request()
+# cgvtime_request()
+# lottime_request()
+# megatime_request()
+
+
+fun()
